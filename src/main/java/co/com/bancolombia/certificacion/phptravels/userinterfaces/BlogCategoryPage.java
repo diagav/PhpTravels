e19ef3411c2e6c8dfc8be61e58e9481ecb052443
Diana Gaviria <DIAGAVIR@pb0b0817353.bancolombia.corp>
2018-08-10 08:10:52 -0500

    2 version

--- src/main/java/co/com/bancolombia/certificacion/phptravels/userinterfaces/BlogCategoryPage.java
+++ src/main/java/co/com/bancolombia/certificacion/phptravels/userinterfaces/BlogCategoryPage.java
@@ -6,6 +6,7 @@ import net.serenitybdd.screenplay.targets.Target;
 
 public class BlogCategoryPage extends PageObject {
 	
+	
 	public static final Target BOTON_ADD = Target.the("boton add").located(By.xpath("//*[@id=\"content\"]/div[1]/div[2]/div[1]/button"));
 	public static final Target CATEGORY_NAME = Target.the("category name").located(By.xpath("//*[@id=\"ADD_BLOG_CAT\"]/div[2]/div/form/div[2]/div[1]/div/input"));
 	public static final Target BOTON_SUBMIT = Target.the("boton submit").located(By.xpath("//*[@id=\"ADD_BLOG_CAT\"]/div[2]/div/form/div[3]/button[2]"));
